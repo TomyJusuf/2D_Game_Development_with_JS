4a00219f062d738fd8abb40277fb1fa24f6ef451
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-01 22:30:58 +0200

    First commit: Installing necessary packages for Webpack bundler & set folders

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i, // For CSS, if needed
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.js$/, // For JavaScript files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: '2D Game JavaScript',
+      template: './index.html',
+    }),
+  ],
+}
