81420279c4ccd6975071e79b29d95e17310886c7
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-03 23:07:01 +0200

    Add method gameOver, drawScore, positionOfWinnerMessage

--- src/app/Components/UI.js
+++ src/app/Components/UI.js
@@ -6,22 +6,60 @@ export default class UI {
     this.color = 'yellow'
   }
   draw(context) {
+    //score
+    this.#drawScore(context)
+
     //ammo
     context.fillStyle = this.color
     for (let i = 0; i < this.game.ammo; i++) {
       context.fillRect(20 + 5 * i, 50, 3, 20)
     }
+
+    //game over messages
+    this.#gameOver(context)
+    context.restore()
   }
 
-  drawScore(context) {
+  #positionOfWinnerMessage(context) {
+    context.save()
+    context.fillStyle = this.color
+    context.shadowOffsetX = 2
+    context.shadowOffsetY = 2
+    context.shadowColor = 'black'
+    context.font = this.fontSite + 'px ' + this.fontFamily
+  }
+
+  #drawScore(context) {
     context.fillStyle = this.color
     context.font = `${this.fontSite}px ${this.fontFamily}`
     context.fillText(`Score: ${this.game.score}`, 20, 20)
   }
 
-  drawGameOver(context) {
-    context.fillStyle = this.color
-    context.font = `${this.fontSite * 2}px ${this.fontFamily}`
-    context.fillText(`Game Over`, 300, 300)
+  #gameOver(context) {
+    this.#positionOfWinnerMessage(context)
+    if (this.game.gameOver) {
+      context.textAlign = 'center'
+      let message1
+      let message2
+      if (this.game.score >= this.game.winningScore) {
+        message1 = 'You Win!'
+        message2 = 'Well Done!'
+      } else {
+        message1 = 'Game Over'
+        message2 = 'Try Again!'
+      }
+      context.font = `100px ${this.fontFamily}`
+      context.fillText(
+        message1,
+        this.game.width * 0.5,
+        this.game.height * 0.5 - 40
+      )
+      context.font = `50px ${this.fontFamily}`
+      context.fillText(
+        message2,
+        this.game.width * 0.5,
+        this.game.height * 0.5 + 40
+      )
+    }
   }
 }
