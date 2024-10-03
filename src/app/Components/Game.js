37e35e4112a6f9554ec048cb4708566aee39f9b1
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-03 22:30:08 +0200

    Create privat method #enemyUpdate, create public method addEnemy and checkCollision

--- src/app/Components/Game.js
+++ src/app/Components/Game.js
@@ -1,7 +1,7 @@
 import Player from './Player'
 import InputHandler from './Inputhandler'
 import UI from './UI'
-
+import Angler from './Enemy'
 class Game {
   constructor(width, height) {
     this.width = width
@@ -9,22 +9,42 @@ class Game {
     this.player = new Player(this)
     this.input = new InputHandler(this)
     this.ui = new UI(this)
+    this.enemies = []
+    this.score = 0
+    this.enemyTimer = 0
+    this.enemyInterval = 1000
     this.ammo = 20 //Limit shooting
     this.maxAmmo = 50
     this.ammoTimer = 0
     this.ammoInterval = 500
-    // this.ammoElapsed = 0
-    // this.gameOver = false
+    this.gameOver = false
   }
   update(deleteTime) {
     this.player.update(this.input)
     this.#ammo_timer(deleteTime)
+    this.#enemyUpdate(deleteTime)
   }
 
   draw(context) {
-    // context == canva.ctx
-    this.player.draw(context)
+    this.player.draw(context) // context == canva.ctx
     this.ui.draw(context)
+    this.ui.drawScore(context)
+    this.enemies.forEach((enemy) => {
+      enemy.draw(context)
+    })
+  }
+  addEnemy() {
+    this.enemies.push(new Angler(this))
+    // console.log(this.enemies)
+  }
+
+  checkCollision(rec1, res2) {
+    return (
+      rec1.x < res2.x + res2.width &&
+      rec1.x + rec1.width > res2.x &&
+      rec1.y < res2.y + res2.height &&
+      rec1.height + rec1.y > res2.y
+    )
   }
 
   #ammo_timer(deleteTime) {
@@ -34,6 +54,39 @@ class Game {
         this.ammoTimer = 0
         this.ammo += 1
       }
+    }
+  }
+
+  #enemyUpdate(deleteTime) {
+    this.enemies.forEach((enemy) => {
+      enemy.update()
+
+      //check collision
+      if (this.checkCollision(this.player, enemy)) {
+        this.gameOver = true
+        enemy.markedForDeletion = true
+      }
+      //check if projectiles hit enemy
+      this.player.projectile.forEach((projectile) => {
+        if (this.checkCollision(projectile, enemy)) {
+          enemy.lives -= 1
+          projectile.markForDeletion = true
+          if (enemy.lives === 0) {
+            enemy.markedForDeletion = true
+            this.score += enemy.score
+            console.log(this.score)
+          }
+        }
+      })
+    })
+
+    //remove dead enemies
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion)
+    if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+      this.addEnemy()
+      this.enemyTimer = 0
+    } else {
+      this.enemyTimer += deleteTime
     }
   }
 }
