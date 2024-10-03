1130a707eaec3288e3321f14569024ee6e74107c
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-03 17:28:48 +0200

    Create private method #ammo_timer, refactor code with  method update()

--- src/app/Components/Game.js
+++ src/app/Components/Game.js
@@ -1,46 +1,40 @@
 import Player from './Player'
-import canva from './Canva'
 import InputHandler from './Inputhandler'
-export class Game {
+import UI from './UI'
+
+class Game {
   constructor(width, height) {
     this.width = width
     this.height = height
     this.player = new Player(this)
-    this.inputHandler = new InputHandler(this)
-    this.ammo = 20
+    this.input = new InputHandler(this)
+    this.ui = new UI(this)
+    this.ammo = 20 //Limit shooting
     this.maxAmmo = 50
     this.ammoTimer = 0
     this.ammoInterval = 500
-    this.ammoElapsed = 0
-    this.gameOver = false
+    // this.ammoElapsed = 0
+    // this.gameOver = false
   }
-  update() {
-    this.player.update(this.inputHandler)
-    if (this.ammoTimer > this.ammoInterval) {
-      if (this.ammo < this.maxAmmo) {
-        this.ammo++
-        this.ammoTimer = 0
-      } else {
-        this.ammoTimer += deleteTime
-      }
-    }
+  update(deleteTime) {
+    this.player.update(this.input)
+    this.#ammo_timer(deleteTime)
   }
 
   draw(context) {
+    // context == canva.ctx
     this.player.draw(context)
+    this.ui.draw(context)
+  }
+
+  #ammo_timer(deleteTime) {
+    if (this.ammo < this.maxAmmo) {
+      this.ammoTimer += deleteTime
+      if (this.ammoTimer > this.ammoInterval) {
+        this.ammoTimer = 0
+        this.ammo += 1
+      }
+    }
   }
 }
-
-const game = new Game(canva.width, canva.height)
-let lastTime = 0
-function animate(timeStamp) {
-  const deleteTime = timeStamp - lastTime
-  lastTime = timeStamp
-  canva.ctx.clearRect(0, 0, canva.width, canva.height)
-  game.update(deleteTime)
-  game.draw(canva.ctx)
-
-  requestAnimationFrame(animate)
-}
-
-animate(0)
+export default Game
