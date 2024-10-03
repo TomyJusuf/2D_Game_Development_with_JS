5924aa7001b1b12cd68ea94dc0861e65fcc2cc76
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-03 17:25:27 +0200

    Created instance and method for UI.js

--- src/app/Components/UI.js
+++ src/app/Components/UI.js
@@ -1,1 +1,15 @@
-class UI {}
+export default class UI {
+  constructor(game) {
+    this.game = game
+    this.fontSite = 20
+    this.fontFamily = 'Helvetica'
+    this.color = 'yellow'
+  }
+  draw(context) {
+    //ammo
+    context.fillStyle = this.color
+    for (let i = 0; i < this.game.ammo; i++) {
+      context.fillRect(20 + 5 * i, 50, 3, 20)
+    }
+  }
+}
