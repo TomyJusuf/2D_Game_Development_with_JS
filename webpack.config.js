b32e575b95bed0c911d923743ca70fe8e4f0684e
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-06 00:14:30 +0200

    Add folder images to assets and load there all my images & set current path into every file which including any image from assets/images/_.png

--- webpack.config.js
+++ webpack.config.js
@@ -43,6 +43,10 @@ module.exports = {
         },
       },
       {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
         test: /\.js$/, // For JavaScript files
         exclude: /node_modules/,
         use: {
