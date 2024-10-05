49bda7ec8bf19344b353b0c1ff879378ca50a2ba
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-05 13:08:25 +0200

    Load images from file imageLoader

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,7 @@
 import '../css/style.css'
 import './Components/Game'
 import { animate } from './Components/Animation'
+import './imageLoader'
 
 window.addEventListener('load', () => {
   animate(0)
