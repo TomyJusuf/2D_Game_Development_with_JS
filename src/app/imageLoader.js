decae75f2aa0ee81c23371f3be363d9faf835288
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-05 14:47:50 +0200

    Add image player & refactor function to appendImages,and in style display none

--- src/app/imageLoader.js
+++ src/app/imageLoader.js
@@ -2,18 +2,22 @@ import layer1 from '../assets/layer1.png'
 import layer2 from '../assets/layer2.png'
 import layer3 from '../assets/layer3.png'
 import layer4 from '../assets/layer4.png'
+import player from '../assets/player.png'
 
 const imageSources = [layer1, layer2, layer3, layer4]
+const playerImagePath = player
 
-export function appendImages(imgPath) {
-  imgPath.forEach((element, index) => {
-    const img = document.createElement('img')
-    const imgName = `layer${index + 1}`
-
-    img.id = imgName
-    img.src = element
+export function appendImages(imgPath, playerSrc) {
+  const createAndAppendImage = (src, id) => {
+    const img = Object.assign(document.createElement('img'), { id, src })
     document.body.appendChild(img)
-  })
+  }
+
+  imgPath.forEach((src, index) =>
+    createAndAppendImage(src, `layer${index + 1}`)
+  )
+
+  createAndAppendImage(playerSrc, 'player')
 }
 
-appendImages(imageSources)
+appendImages(imageSources, playerImagePath)
