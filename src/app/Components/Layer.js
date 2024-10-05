a05938c9f3674fa378a0ee1c954530ea82d97c57
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-05 14:22:36 +0200

    Add backgrounds layers to game background board

--- src/app/Components/Layer.js
+++ src/app/Components/Layer.js
@@ -1,1 +1,25 @@
-class Layer {}
+export default class Layer {
+  constructor(game, imageSrc, speedModifier) {
+    this.game = game
+    this.image = new Image()
+    this.image.src = imageSrc
+
+    this.speedModifier = speedModifier
+    this.width = 1768
+    this.height = 500
+    this.x = 0
+    this.y = 0
+  }
+  update() {
+    if (this.x <= -this.width) this.x = 0
+    this.x -= this.game.speed * this.speedModifier
+  }
+  draw(context) {
+    if (this.image.complete) {
+      context.drawImage(this.image, this.x, this.y)
+      context.drawImage(this.image, this.x + this.width, this.y)
+    } else {
+      console.warn(`Image not loaded: ${this.image.src}`)
+    }
+  }
+}
