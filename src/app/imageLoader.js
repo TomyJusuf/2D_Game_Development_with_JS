fe8e9540574f7b4bc718d21ec3387863f32a7759
Tomas Matusek <82041257+TomyJusuf@users.noreply.github.com>
2024-10-05 23:03:47 +0200

    Refactor function that add id and src to img, base on imagesObj

--- src/app/imageLoader.js
+++ src/app/imageLoader.js
@@ -3,21 +3,32 @@ import layer2 from '../assets/layer2.png'
 import layer3 from '../assets/layer3.png'
 import layer4 from '../assets/layer4.png'
 import player from '../assets/player.png'
+import angler1 from '../assets/angler1.png'
+import angler2 from '../assets/angler2.png'
+import lucky from '../assets/lucky.png'
 
-const imageSources = [layer1, layer2, layer3, layer4]
-const playerImagePath = player
-
-export function appendImages(imgPath, playerSrc) {
-  const createAndAppendImage = (src, id) => {
-    const img = Object.assign(document.createElement('img'), { id, src })
-    document.body.appendChild(img)
-  }
-
-  imgPath.forEach((src, index) =>
-    createAndAppendImage(src, `layer${index + 1}`)
-  )
-
-  createAndAppendImage(playerSrc, 'player')
+const imagesObj = {
+  angler1,
+  angler2,
+  lucky,
+  player,
+  layer1,
+  layer2,
+  layer3,
+  layer4,
 }
 
-appendImages(imageSources, playerImagePath)
+export function appendImages(newObj) {
+  const createAndAppendImage = (newObj) => {
+    for (const idKey in newObj) {
+      const id = idKey
+      const src = newObj[idKey]
+      const img = Object.assign(document.createElement('img'), { id, src })
+      document.body.appendChild(img)
+    }
+  }
+
+  createAndAppendImage(newObj)
+}
+
+appendImages(imagesObj)
